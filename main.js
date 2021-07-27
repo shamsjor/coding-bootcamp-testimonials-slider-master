4deb41bcfc99751e2ceffe0d7bc1d2edc18e45e2
shamsjor <74854926+shamsjor@users.noreply.github.com>
2021-07-27 08:32:32 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let move = document.querySelector(".slice");
+let person = document.querySelectorAll("div.person");
+console.log(person);
+document.addEventListener("click", (event) => {
+  if (event.target.textContent === `` && event.target.nodeName === `DIV`) {
+    for (let i = 0; i < person.length; i++) {
+      if (person[i].getAttribute(`style`) === `display: none;`) {
+        person[i].style.display = "block";
+      } else if (person[i].getAttribute(`style`) === `display: block;`) {
+        person[i].style.display = "none";
+      }
+    }
+  }
+});
